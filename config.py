4554ceeb8c58fa0e71f23e3a8ebcf6c502054d4d
alnawei <122384898+alnawei@users.noreply.github.com>
2026-07-08 19:19:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
-SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
-SENDER_EMAIL = os.getenv("SENDER_EMAIL")
-SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
+RESEND_API_KEY = os.getenv("RESEND_API_KEY")
 RECIPIENT = os.getenv("RECIPIENT")
 
 ALIYUN_ACCESS_KEY_ID = os.getenv("ALIYUN_ACCESS_KEY_ID")
